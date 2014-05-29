abdd04f5cd7cc5579ff4aa9c6e5461b40ba83ca7
Kevin John <kj@universe.sonoma.edu>
2014-05-29 17:00:57 +0000

    removed zip files and updated install script

--- Install.sh
+++ Install.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-unzip LearningByMaking.zip
 sudo cp -r javalogo/ /usr/share/javalogo
 sudo chmod 777 LearningByMaking/ulogo/run.sh
-unzip tatext.zip -d TurtleLogo
-unzip CGM101V14.zip -d Oscilloscope
 cd Oscilloscope
 sudo sh Install-Linux-64-bit.sh
 sudo chmod 777 CGM101-linux-x86-64
