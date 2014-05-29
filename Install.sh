424cd9c47cc4aff67038010fb26cf07b3f4c2b1f
Kevin John <kj@universe.sonoma.edu>
2014-05-29 17:35:27 +0000

    fixed javalogo location on install script

--- Install.sh
+++ Install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo cp -r javalogo/ /usr/share/javalogo
+sudo cp -r LearningByMaking/javalogo/ /usr/share/javalogo
 sudo chmod 777 LearningByMaking/ulogo/run.sh
 cd Oscilloscope
 sudo sh Install-Linux-64-bit.sh
