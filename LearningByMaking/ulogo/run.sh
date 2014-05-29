c6ae111bd66d56edd5c641308d565a061eabbe8a
Kevin John <kj@universe.sonoma.edu>
2014-05-29 03:07:24 +0000

    removed expanded folders

--- LearningByMaking/ulogo/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -o history
-set -f
-sudo chmod 777 dev/ttyUSB*
-(while true; do read -e lastcmd; history -s $lastcmd; echo $lastcmd; done) | java -jar /usr/share/javalogo/jl.jar "$@"
-
--- Oscilloscope/stub.tcl
+++
@@ -1,7 +0,0 @@
-package require math::fourier
-package require BWidget
-package require Img
-package require Tktable
-package require http
-
-source main.tcl
