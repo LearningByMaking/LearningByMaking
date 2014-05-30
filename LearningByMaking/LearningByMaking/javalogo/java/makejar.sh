41546bb55eb43845d4fe74815ac441c2315a040f
Kevin John <kj@universe.sonoma.edu>
2014-05-30 20:18:53 +0000

    removed extra nested copy of LearningByMaking folder, updated the run script to manage usb connection better

--- LearningByMaking/LearningByMaking/javalogo/java/makejar.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-rm -f *.class
-javac *.java
-jar -cmf manifest.mf ../jl.jar *.class *.java makejar.sh pcmakejar.bat manifest.mf
-rm *.class
--- LearningByMaking/LearningByMaking/javalogo/jl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -o history
-set -f
-(while true; do read -e lastcmd; history -s $lastcmd; echo $lastcmd; done) | java -jar jl.jar
-
