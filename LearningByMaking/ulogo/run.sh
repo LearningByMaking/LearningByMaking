0a6ceccb7b5b124cf8ba870d58d931ab6c2cdd34
cperuta <cperuta@nasa-169-38.sonoma.edu>
2014-06-04 15:15:26 -0700

    removing USB chmod commands

--- LearningByMaking/ulogo/run.sh
+++ LearningByMaking/ulogo/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -o history
 set -f
-sudo chmod 777 /dev/ttyUSB*
+#sudo chmod 777 /dev/ttyUSB*
 (while true; do read -e lastcmd; history -s $lastcmd; echo $lastcmd; done) | java -jar /usr/share/javalogo/jl.jar "$@"
 
--- Oscilloscope.sh
+++ Oscilloscope.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd Oscilloscope
-sudo chmod 777 /dev/*USB*
+#sudo chmod 777 /dev/*USB*
 ./CGR101-linux-x86-64
